ed78ac7b99cb3461756b09846c95f1a87cffaa50
nghiabm <nghiabm@xdp.vn>
2025-12-15 21:50:35 +0700

    update next

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
+export const middlewareConfig = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
+
+export default middlewareConfig;
